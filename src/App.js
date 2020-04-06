f9b5249e6d0e40e432f9df9755c056a75dc6f8dc
Nicholas_Huang713 <nhuang713@gmail.com>
2020-04-06 02:45:23 -0700

    materialui practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/styles';
+import {Grid} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  helloThereStyle: {
+    fontStyle: 'oblique'
+  }
+});
 
 function App() {
+  // const classes = useStyles;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction="column">
+      <Header />
+      {/* <Grid item>Header Content</Grid> */}
+      <Grid item container>
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={12} sm={8}>Body ContentBody 
+        ContentBody ContentBody ContentBody ContentBody ContentBody 
+        ContentBody ContentBody ContentBody ContentBody Content</Grid>
+        <Grid item xs={0} sm={2}/>
+      </Grid>
+    </Grid>
+
+
+
+
+    // <div>
+    //   <NavBar />
+    //   <Button variant="outlined" color="primary">
+    //     Hello World
+    //   </Button>
+
+    //   <Typography 
+    //     className={classes.helloThereStyle}
+    //     variant="h1"
+    //     color="primary"
+    //   >
+    //     Helloooo
+    //   </Typography>
+    // </div>
   );
 }
 
